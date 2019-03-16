2aa2a4f6cce008078d27e2a60f2b44caf412170f
Robin Eßmann <robin@robinfedora.fritz.box>
2019-03-16 19:03:31 +0100

    Interpreter using Reverse Polish Notation

--- interpretwo.ml
+++ interpretwo.ml
@@ -1,200 +1,357 @@
 (* A nicer interpreter. In my opinion. *)
+module Interpreter = struct
 
-type token =
-  | NAME of string
-  | INT of int
-  | STR of string
-  | LET
-  | PRINT
-  | NEWLINE
-  | ERROR
-  | PLUS
+  let debug = false
 
-exception InvalidToken
+  type token =
+    | NAME of string
+    | INT of int
+    | STR of string
+    | LET
+    | PRINT
+    | NEWLINE
+    | ERROR
+    | PLUS
+    | MINUS
+    | UNOP_MINUS
+    | MULT
+    | DIV
+    | START
+    | END
 
-let interpreter (tokens : token list) = (
-  let rec get_stack (stack : token list) = (
-    function
-    | NEWLINE :: tl -> (
-      stack, tl
-    )
-    | ERROR :: tl -> (
-      print_endline "Error at get_stack!";
-      raise InvalidToken
-    )
-    | hd :: tl -> (
-      get_stack (hd :: stack) tl
-    )
-    | [] -> (
-      stack, []
-    )
-  ) in
-  let rec get_token (vars : (string * token) list) = (
-    function
-    | a :: PLUS :: b :: tl -> (
-      let a, b = get_token vars [a], get_token vars [b] in
-      let hd = (
-        match a, b with
-        | INT a, INT b -> (
-          INT (a + b)
-        )
-        | _ -> (
-          ERROR
+  exception InvalidToken
+
+  let interpreter (tokens : token list) = (
+    let rec rpn (input : token list) (stack : token list) (output : token list) = (
+      let rec precedence (op : token) (stack : token list) (output : token list) = (
+        match stack with
+        | [] ->
+          [op], output
+        | stack_op :: tl -> (
+          match op, stack_op with
+          | END, START ->
+            tl, output
+          | MULT, LET
+          | MULT, START
+          | MULT, PRINT
+          | MULT, PLUS
+          | MULT, MINUS
+          | DIV, PLUS
+          | DIV, MINUS
+          | DIV, LET
+          | DIV, START
+          | DIV, PRINT
+          | PLUS, LET
+          | PLUS, START
+          | PLUS, PRINT
+          | MINUS, LET
+          | MINUS, START
+          | MINUS, PRINT
+          | UNOP_MINUS, LET
+          | UNOP_MINUS, START
+          | UNOP_MINUS, PRINT
+          | UNOP_MINUS, MULT
+          | UNOP_MINUS, DIV
+          | START, _ ->
+            (op :: stack), output
+          | MULT, _
+          | DIV, _
+          | END, _
+          | PRINT, _
+          | PLUS, _
+          | MINUS, _
+          | UNOP_MINUS, _
+          | LET, _ ->
+            precedence op tl (stack_op :: output)
+          | _ -> (
+            raise InvalidToken
+          )
         )
       ) in
-      get_token vars (hd :: tl)
-    )
-    | [INT i] -> (
-      INT i
-    )
-    | [STR s] -> (
-      STR s
-    )
-    | [NAME n] -> (
-      List.assoc n vars
-    )
-    | _ -> (
-      ERROR
-    )
-  ) in
-  let printer = (
-    function
-    | INT i -> (
-      print_int i;
-      print_newline ()
-    )
-    | STR s -> (
-      print_endline s
-    )
-    | _ -> (
-      print_endline "Error at printer!";
-      raise InvalidToken
-    )
-  ) in
-  let rec iter (vars : (string * token) list) = (
-    function
-    | PRINT :: tl -> (
-      let stack, tl = get_stack [] tl in
-      printer (get_token vars stack);
-      iter vars tl
-    )
-    | LET :: NAME n :: tl -> (
-      let stack, tl = get_stack [] tl in
-      iter ((n, (get_token vars stack)) :: (List.remove_assoc n vars)) tl
-    )
-    | NEWLINE :: tl -> (
-      iter vars tl
-    )
-    | [] -> (
-
-    )
-    | _ -> (
-      print_endline "Error at iter!";
-      raise InvalidToken
-    )
-  ) in
-  iter [] tokens
-)
-
-let tokenizer (stack : char list) = (
-  let rec ignore_token = (
-    function
-    | NEWLINE :: stack -> (
-      NEWLINE :: stack
-    )
-    | hd :: tl -> (
-      ignore_token tl
-    )
-    | [] -> (
+      match input with
+      | NEWLINE :: _
+      | [] -> (
+        match stack with
+        | [] -> (List.rev output), input
+        | hd :: tl -> rpn input tl (hd :: output)
+      )
+      | NAME _ :: tl
+      | STR _ :: tl
+      | INT _ :: tl ->
+        let elem = List.hd input in
+        rpn tl stack (elem :: output)
+      | op :: tl ->
+        let stack, output = precedence op stack output in
+        rpn tl stack output
+    ) in
+    let rec eval_rpn (input : token list) (vars : (string * token) list) (stack : token list) = (
+      match input with
+      | NAME _ :: tl
+      | STR _ :: tl
+      | INT _ :: tl ->
+        eval_rpn tl vars ((List.hd input) :: stack)
+      | op :: input -> (
+        match op with
+        | LET -> (
+          match stack with
+          | v :: NAME n :: stack -> (
+            eval_rpn input ((n, v) :: (List.remove_assoc n vars)) stack
+          )
+          | _ -> raise InvalidToken
+        )
+        | PRINT -> (
+          match stack with
+          | NAME a :: stack -> (
+            let v = List.assoc a vars in
+            eval_rpn (PRINT :: input) vars (v :: stack)
+          )
+          | STR a :: stack -> (
+            print_endline a;
+            eval_rpn input vars stack
+          )
+          | INT a :: stack -> (
+            print_int a;
+            print_newline ();
+            eval_rpn input vars stack
+          )
+          | _ -> raise InvalidToken
+        )
+        | PLUS -> (
+          match stack with
+          | INT b :: INT a :: stack -> (
+            eval_rpn input vars ((INT (a + b)) :: stack)
+          )
+          | STR b :: STR a :: stack -> (
+            eval_rpn input vars ((STR (a ^ b)) :: stack)
+          )
+          | NAME a :: stack -> (
+            let v = List.assoc a vars in
+            eval_rpn (PLUS :: input) vars (v :: stack)
+          )
+          | b :: NAME a :: stack -> (
+            let v = List.assoc a vars in
+            eval_rpn (PLUS :: input) vars (b :: v :: stack)
+          )
+          | _ -> raise InvalidToken
+        )
+        | MINUS -> (
+          match stack with
+          | INT b :: INT a :: stack -> (
+            eval_rpn input vars ((INT (a - b)) :: stack)
+          )
+          | NAME a :: stack -> (
+            let v = List.assoc a vars in
+            eval_rpn (MINUS :: input) vars (v :: stack)
+          )
+          | b :: NAME a :: stack -> (
+            let v = List.assoc a vars in
+            eval_rpn (MINUS :: input) vars (b :: v :: stack)
+          )
+          | _ -> raise InvalidToken
+        )
+        | UNOP_MINUS -> (
+          match stack with
+          | INT a :: stack -> (
+            eval_rpn input vars ((INT (-a)) :: stack)
+          )
+          | NAME a :: stack -> (
+            let v = List.assoc a vars in
+            eval_rpn (UNOP_MINUS :: input) vars (v :: stack)
+          )
+          | _ -> raise InvalidToken
+        )
+        | MULT -> (
+          match stack with
+          | INT b :: INT a :: stack -> (
+            eval_rpn input vars ((INT (a * b)) :: stack)
+          )
+          | NAME a :: stack -> (
+            let v = List.assoc a vars in
+            eval_rpn (MULT :: input) vars (v :: stack)
+          )
+          | b :: NAME a :: stack -> (
+            let v = List.assoc a vars in
+            eval_rpn (MULT :: input) vars (b :: v :: stack)
+          )
+          | _ -> (
+            raise InvalidToken
+          )
+        )
+        | DIV -> (
+          match stack with
+          | INT b :: INT a :: stack -> (
+            eval_rpn input vars ((INT (a / b)) :: stack)
+          )
+          | NAME a :: stack -> (
+            let v = List.assoc a vars in
+            eval_rpn (DIV :: input) vars (v :: stack)
+          )
+          | b :: NAME a :: stack -> (
+            let v = List.assoc a vars in
+            eval_rpn (DIV :: input) vars (b :: v :: stack)
+          )
+          | _ -> raise InvalidToken
+        )
+        | _ -> raise InvalidToken
+      )
+      | [] -> (
+        vars
+      )
+    ) in
+    let rec iterate (input : token list) (vars : (string * token) list) = (
+      match input with
+      | [] -> ()
+      | NEWLINE :: input -> iterate input vars
+      | _ -> (
+        let expr, input = rpn input [] [] in
+        let vars = eval_rpn expr vars [] in
+        iterate input vars
+      )
+    ) in
+    let rec debug_iter (input : token list) (vars : (string * token) list) (output : token list) = (
+      match input with
+      | [] -> output
+      | NEWLINE :: input -> debug_iter input vars output
+      | _ -> (
+        let expr, input = rpn input [] [] in
+        try (
+          let vars = eval_rpn expr vars [] in
+          debug_iter input vars (List.append output expr)
+        ) with InvalidToken -> (
+          print_endline "Failed!";
+          List.append output expr
+        )
+      )
+    ) in
+    if debug then (
+      debug_iter tokens [] []
+    ) else (
+      iterate tokens [];
       []
     )
-  ) in
-  let rec int_token (buffer : string) = (
-    function
-    | hd :: tl when hd >= '0' && hd <= '9' -> (
-      int_token ((String.make 1 hd) ^ buffer) tl
-    )
-    | stack -> (
-      INT (int_of_string buffer), stack
-    )
-  ) in
-  let rec str_token (buffer : string) = (
-    function
-    | '"' :: stack -> (
-      STR buffer, stack
-    )
-    | hd :: tl -> (
-      str_token ((String.make 1 hd) ^ buffer) tl
-    )
-    | [] -> (
-      ERROR, []
-    )
-  ) in
-  let rec name_token (buffer : string) = (
-    function
-    | ' ' :: stack -> (
-      NAME buffer, stack
-    )
-    | hd :: tl when hd >= 'A' && hd <= 'Z' -> (
-      name_token ((String.make 1 hd) ^ buffer) tl
-    )
-    | _ :: stack -> (
-      ERROR, stack
-    )
-    | [] -> (
-      ERROR, []
-    )
-  ) in
-  let rec main_parser (buffer : token list) = (
-    function
-    | '\n' :: stack -> (
-      main_parser (NEWLINE :: buffer) stack
-    )
-    | '+' :: stack -> (
-      main_parser (PLUS :: buffer) stack
-    )
-    | '#' :: stack -> (
-      main_parser (ignore_token buffer) stack
-    )
-    | ' ' :: stack -> (
-      main_parser buffer stack
-    )
-    | '"' :: stack -> (
-      let tok, stack = str_token "" stack in
-      main_parser (tok :: buffer) stack
-    )
-    | 'P' :: tl -> (
-      main_parser (PRINT :: buffer) tl
-    )
-    | 'T' :: 'E' :: 'L' :: tl -> (
-      main_parser (LET :: buffer) tl
-    )
-    | c :: stack when c >= '0' && c <= '9' -> (
-      let tok, stack = int_token "" (c :: stack) in
-      main_parser (tok :: buffer) stack
-    )
-    | c :: stack -> (
-      let tok, stack = name_token "" (c :: stack) in
-      main_parser (tok :: buffer) stack
-    )
-    | [] -> (
-      buffer
-    )
-  ) in
-  main_parser [] stack
-)
+  )
 
-let char_stack (f : in_channel) = (
-  let rec loop stack = (
-    try (
-      let c = input_char f in
-      loop (c :: stack)
-    ) with End_of_file -> (
-      stack
-    )
-  ) in
-  loop []
-)
+  let tokenizer (stack : char list) = (
+    let rec ignore_token = (
+      function
+      | NEWLINE :: stack -> (
+        NEWLINE :: stack
+      )
+      | hd :: tl -> (
+        ignore_token tl
+      )
+      | [] -> (
+        []
+      )
+    ) in
+    let rec int_token (buffer : string) = (
+      function
+      | hd :: tl when hd >= '0' && hd <= '9' -> (
+        int_token ((String.make 1 hd) ^ buffer) tl
+      )
+      | stack -> (
+        INT (int_of_string buffer), stack
+      )
+    ) in
+    let rec str_token (buffer : string) = (
+      function
+      | '"' :: stack -> (
+        STR buffer, stack
+      )
+      | hd :: tl -> (
+        str_token ((String.make 1 hd) ^ buffer) tl
+      )
+      | [] -> (
+        ERROR, []
+      )
+    ) in
+    let rec name_token (buffer : string) = (
+      function
+      | ' ' :: stack -> (
+        NAME buffer, stack
+      )
+      | hd :: tl when hd >= 'A' && hd <= 'Z' -> (
+        name_token ((String.make 1 hd) ^ buffer) tl
+      )
+      | _ :: stack -> (
+        ERROR, stack
+      )
+      | [] -> (
+        ERROR, []
+      )
+    ) in
+    let rec main_parser (buffer : token list) = (
+      function
+      | '\n' :: stack -> (
+        main_parser (NEWLINE :: buffer) stack
+      )
+      | ')' :: stack -> (
+        main_parser (END :: buffer) stack
+      )
+      | '(' :: stack
+      | '+' :: '(' :: stack -> (
+        main_parser (START :: buffer) stack
+      )
+      | '-' :: '(' :: stack -> (
+        main_parser (START :: UNOP_MINUS :: buffer) stack
+      )
+      | '-' :: stack -> (
+        main_parser (MINUS :: buffer) stack
+      )
+      | '+' :: stack -> (
+        main_parser (PLUS :: buffer) stack
+      )
+      | '*' :: stack -> (
+        main_parser (MULT :: buffer) stack
+      )
+      | '/' :: stack -> (
+        main_parser (DIV :: buffer) stack
+      )
+      | '#' :: stack -> (
+        main_parser (ignore_token buffer) stack
+      )
+      | ' ' :: stack -> (
+        main_parser buffer stack
+      )
+      | '"' :: stack -> (
+        let tok, stack = str_token "" stack in
+        main_parser (tok :: buffer) stack
+      )
+      | 'P' :: tl -> (
+        main_parser (PRINT :: buffer) tl
+      )
+      | 'T' :: 'E' :: 'L' :: tl -> (
+        main_parser (LET :: buffer) tl
+      )
+      | c :: stack when c >= '0' && c <= '9' -> (
+        let tok, stack = int_token "" (c :: stack) in
+        main_parser (tok :: buffer) stack
+      )
+      | c :: stack -> (
+        let tok, stack = name_token "" (c :: stack) in
+        main_parser (tok :: buffer) stack
+      )
+      | [] -> (
+        buffer
+      )
+    ) in
+    main_parser [] stack
+  )
+
+  let char_stack (f : in_channel) = (
+    let rec loop stack = (
+      try (
+        let c = input_char f in
+        loop (c :: stack)
+      ) with End_of_file -> (
+        stack
+      )
+    ) in
+    loop []
+  )
+end
+
+open Interpreter
 
 let _ = (
   open_in "test"
